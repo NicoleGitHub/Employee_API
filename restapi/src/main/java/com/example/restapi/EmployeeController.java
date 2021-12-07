4948e73f86238bb3cb3f98b1753f5e3843f7475f
pangni <94227117+NicoleGitHub@users.noreply.github.com>
2021-12-07 17:31:53 +0800

    [employeeApi][Klaus] -feat: add getAllEmployeesByID(Integer ID)

--- restapi/src/main/java/com/example/restapi/EmployeeController.java
+++ restapi/src/main/java/com/example/restapi/EmployeeController.java
@@ -1,8 +1,6 @@
 package com.example.restapi;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,10 @@ public class EmployeeController {
     @GetMapping
     public List<Employee> getAllEmployees() {
         return employeeRepository.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeByID(@PathVariable Integer id) {
+        return employeeRepository.getEmployeesByID(id);
     }
 }
--- restapi/src/main/java/com/example/restapi/EmployeeRepository.java
+++ restapi/src/main/java/com/example/restapi/EmployeeRepository.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class EmployeeRepository {
@@ -15,5 +16,10 @@ public class EmployeeRepository {
 
     public List<Employee> getAll() {
         return employees;
+    }
+
+    public Employee getEmployeesByID(Integer id){
+        System.out.print(id);
+        return employees.stream().filter(employee -> employee.getId().equals(id)).findFirst().orElse(null);
     }
 }
